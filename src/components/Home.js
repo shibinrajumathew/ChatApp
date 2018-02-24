ad2c66f4804616a7d4f4b533ba1ad283868ceab2
Shibin Raju Mathew <shibinrajumathew@users.noreply.github.com>
2018-02-24 22:18:12 +0530

    src commit

---
+++ src/components/Home.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+import { Actions } from 'react-native-router-flux';
+
+export default class Home extends React.Component {
+  state = {
+    name: '',
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={[styles.label, {marginTop: 40}]}>
+          Enter your name :
+        </Text>
+        <TextInput
+          placeholder='Type your name here'
+          style={styles.textInput}
+          onChangeText={(text) => {
+            this.setState({
+              name: text,
+            });
+          }}
+          value={this.state.name}
+        />
+
+        <TouchableOpacity
+          onPress={() => {
+            Actions.chat({
+              sname: this.state.name,
+            });
+          }}
+        >
+          <Text style={styles.label}>
+            Next
+          </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  label: {
+    fontSize: 20,
+    marginLeft: 15,
+  },
+  textInput: {
+    height: 40,
+    marginLeft: 15,
+  },
+});
